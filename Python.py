e40b3093fd8cdbc5f7f46d0a9f4956ab942dc723
PanupongSoongwiriya <61070345@kmitl.ac.th>
2018-11-28 20:14:32 +0700

    Update Python.py

--- Python.py
+++ Python.py
@@ -3,52 +3,69 @@ import csv
 def function():
     """ Something """
     print('Let Do IT')
-    #DC
     file = open('rate_of_dc.csv')
     data = csv.reader(file)
-    table_dc = []
-    table_view_dc = {"2005":"---", "2006":"---", "2007":"---", "2008":"---", "2009":"---",\
-     "2010":"---", "2011":"---", "2012":"---", "2013":"---", "2014":"---", "2015":"---", "2016":"---",\
-      "2017":"---", "2018":"---"}
-    check_view_dc = ""
-    for i in data:
-        if 'date' not in i:
-            table_dc += [[*(i[:2]), i[-1]]]
-        for j in i[-1]:
-            if j != ",":
-                check_view_dc += j
-        if i[1] != 'date' and i[1] in table_view_dc:
-            if table_view_dc[i[1]] != "---":
-                table_view_dc[i[1]] = (int(check_view_dc)+table_view_dc[i[1]])//2
-            else:
-                table_view_dc[i[1]] = int(check_view_dc)
-        check_view_dc = ""
-    print(*(table_dc), sep="\n")
-    print()
-    for i in table_view_dc:
-        print(i, table_view_dc[i])
-    #Marvel
+    set_check = set()
+    view = dict()
+    for year in data:
+        if "date" not in year:
+            set_check.add(int(year[1]))
     file = open('rate_of_marvel.csv')
     data = csv.reader(file)
-    table_marvel = []
-    table_view_marvel = {"2005":"---", "2006":"---", "2007":"---", "2008":"---", "2009":"---",\
-     "2010":"---", "2011":"---", "2012":"---", "2013":"---", "2014":"---", "2015":"---", "2016":"---",\
-      "2017":"---", "2018":"---"}
-    check_view_marvel = ""
+    for year in data:
+        if "date" not in year:
+            set_check.add(int(year[1]))
+    for year in range(min(set_check), max(set_check)+1):
+        view[str(year)] = "-"
+    calculate_view('rate_of_dc.csv', view, [])
+    view1 = dict()
+    for i in view:
+        view1[i] = "-"
+    calculate_rate('rate_of_dc.csv', view1, [])
+    for i in view1:
+        view[i] = "-"
+    calculate_view('rate_of_marvel.csv', view, [])
+    for i in view:
+        view1[i] = "-"
+    calculate_rate('rate_of_dc.csv', view1, [])
+
+def calculate_view(name, view, table):
+    print('view')
+    file = open(name)
+    data = csv.reader(file)
+    check_view = ""
     for i in data:
         if 'date' not in i:
-            table_marvel += [[*(i[:2]), i[-1]]]
-        for j in i[-1]:
-            if j != ",":
-                check_view_marvel += j
-        if i[1] != 'date' and i[1] in table_view_marvel:
-            if table_view_marvel[i[1]] != "---":
-                table_view_marvel[i[1]] = (int(check_view_marvel)+table_view_marvel[i[1]])//2
+            table += [[*(i[:2]), i[-1]]]
+        for num in i[-1]:
+            if num != ",":
+                check_view += num
+        if i[1] != 'date' and i[1] in view:
+            if view[i[1]] != "-":
+                view[i[1]] = (int(check_view)+view[i[1]])//2
             else:
-                table_view_marvel[i[1]] = int(check_view_marvel)
-        check_view_marvel = ""
-    print(*(table_marvel), sep="\n")
+                view[i[1]] = int(check_view)
+        check_view = ""
+    print(*(table), sep="\n")#Output
     print()
-    for i in table_view_marvel:
-        print(i, table_view_marvel[i])
+    for i in view:#Output
+        print(i, view[i])
+
+def calculate_rate(name, view, table):
+    print('rate')
+    file = open(name)
+    data = csv.reader(file)
+    check_view = 0
+    for i in data:
+        if i[2] != 'rate':
+            check_view = float(i[2])
+        if i[1] != 'date' and i[1] in view:
+            if view[i[1]] != "-":
+                view[i[1]] = (check_view+view[i[1]])//2
+            else:
+                view[i[1]] = check_view
+        check_view = 0
+    for i in view:#Output
+        print(i, view[i])
+
 function()
