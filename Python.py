ca3ba6bae25822927ad8210f4fe6124f9fd407c0
PanupongSoongwiriya <61070345@kmitl.ac.th>
2018-11-28 23:27:33 +0700

    เพิ่ม ตาราง Rate + แก้ไขเรื่องทศนิยม

--- Python.py
+++ Python.py
@@ -19,41 +19,47 @@ def find_year(name_file, set_check):
             set_check.add(int(year[1]))
     return set_check
 
-def calculate_view(name_file, view, rate, table, name):
-    """Data analysis and data extraction."""
-    file = open(name_file)
-    data = csv.reader(file)
-    check_rate, calculate_view = 0, ""
-    for i in data:
-        if 'date' not in i:
-            check_rate = float(i[2])
-            table += [[*(i[:2]), i[-1]]]
-        for num in i[-1]:
-            if num != ",":
-                check_view += num
-        if i[1] in view:
-            if view[i[1]] != "-":
-                view[i[1]] = (int(check_view)+view[i[1]])//2
-                rate[i[1]] = (check_rate+rate[i[1]])//2
-            else:
-                view[i[1]] = int(check_view)
-                rate[i[1]] = check_rate
-        check_view, check_rate = "", 0
-    print()
-    print('Table', name)
-    print(*(table), sep="\n")#Output
-    print('View', name)
-    for i in view:#Output
-        print(i, view[i])
-    print('Rate', name)
-    for i in rate:#Output
-        print(i, rate[i])
-
 def reset_view(view):
     """Remain the same value Need to reset."""
     view1 = dict()
     for i in view:
         view1[i] = "-"
     return view1
+
+def calculate_view(name_file, view, rate, table_view, name):
+    """Data analysis and data extraction."""
+    file = open(name_file)
+    data = csv.reader(file)
+    check_rate, calculate_view, table_rate = 0, "", []
+    for i in data:
+        if 'date' not in i:
+            check_rate = float(i[2])
+            table_view += [[*(i[:2]), i[-1]]]
+            table_rate += [i[:3]]
+        for num in i[-1]:
+            if num != ",":
+                check_view += num
+        if i[1] in view:
+            if view[i[1]] != "-":
+                view[i[1]] = (int(check_view)+view[i[1]])//2
+                rate[i[1]] = (check_rate+rate[i[1]])/2
+            else:
+                view[i[1]] = int(check_view)
+                rate[i[1]] = check_rate
+        check_view, check_rate = "", 0
+    print()
+    print('Table view', name)
+    print(*(table_view), sep="\n")#Output
+    print('View', name)
+    for i in view:#Output
+        print(i, view[i])
+    print('Table rate', name)
+    print(*(table_rate), sep="\n")#Output
+    print('Rate', name)
+    for i in rate:#Output
+        if rate[i] != "-":
+            print(i, "%.1f"%(rate[i]))
+        else:
+            print(i, rate[i])
 
 function(set(), dict())
