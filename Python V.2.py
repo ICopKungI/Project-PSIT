f79833f8936be96d9f91477abdb8ddebc78a1a8d
PanupongSoongwiriya <61070345@kmitl.ac.th>
2018-12-02 14:07:54 +0700

    ย่อโค้ด และ คอมเมนเป็นภาษาไทย

---
+++ Python V.2.py
@@ -0,0 +1,87 @@
+""" PROJECT PSIT 2018 """
+"""หมายเหตุ V.2 นำดีบัคของ V.1 ออกและ***น่าจะ***เป็นโค้ดที่สมบูรณ์ที่สุดแล้ว"""
+import csv
+import pygal #หมายเหตุ ต้องติดตั้งโปรแกรมเพิ่มเติมถึงจะรัน imprt plgal ได้
+def main(set_check, analyze, view_dc, rate_dc):
+    """ Sol """
+    view_marvel, rate_marvel = dict(), dict()
+
+    print('งานการมี แต่ไม่ทำ')
+
+    find_year('rate_of_dc.csv', set_check)
+    find_year('rate_of_marvel.csv', set_check)
+    for year in range(min(set_check), max(set_check)+1):#เรียงลำดับปีตั้งแต่ปีแรกจนถึงปีล่าสุดที่มีหนังเข้าโรง
+        analyze[str(year)] = None
+    view_dc, rate_dc = separate('rate_of_dc.csv', analyze, reset_analyze(analyze), [], "DC")
+    view_dc, rate_dc = list(view_dc.values()), list(rate_dc.values())
+
+    view_marvel, rate_marvel = separate('rate_of_marvel.csv', reset_analyze(analyze), reset_analyze(analyze), [], "Marvel")
+    view_marvel, rate_marvel = list(view_marvel.values()), list(rate_marvel.values())
+
+    graph(set_check, view_dc, view_marvel, 'view.svg', 'view_all.svg')
+    graph(set_check, rate_dc, rate_marvel, 'rate.svg', 'rate_all.svg')
+
+def find_year(name_file, set_check):
+    """Find a year."""
+    """หาปีที่ค่าย DC กับ Marvel เข้าปีแรกในจนถึงปัจจุบัน"""
+    file = open(name_file)
+    data = csv.reader(file)
+    for year in data:
+        if "date" not in year:
+            set_check.add(int(year[1]))
+    return set_check
+
+def reset_analyze(analyze):
+    """Remain the same value Need to reset."""
+    """View ยังคงมีค่าเก่าที่คิดไปแล้วอยู่จึงต้องทำให้Viewกลับมาเป็น Noneเพื่อเอาไปใช้ต่อ"""
+    analyze1 = dict()
+    for i in analyze:
+        analyze1[i] = None
+    return analyze1
+
+def separate(name_file, analyze, rate, table_view, name):
+    """Data analysis and data extraction."""
+    file = open(name_file)
+    data = csv.reader(file)
+    check_rate, check_view, table_rate = 0, "", []
+    for i in data:#ลูปแยก Rate กับ View
+        if 'date' not in i:
+            check_rate = float(i[2])
+            table_view += [[*(i[:2]), i[-1]]]
+            table_rate += [i[:3]]
+        for num in i[-1]:#เอา "," ออกจากยอด View
+            if num != ",":
+                check_view += num
+        if i[1] in analyze:
+            if analyze[i[1]] != None:#ในกรณีที่ใน1ปีมีมากกว่า 1 เรื่อง
+                analyze[i[1]] += int(check_view)
+                rate[i[1]] = float("%.1f"%((check_rate+rate[i[1]])/2))#เฉลี่ย Rate
+            else:#นำยอด View และ Rate เข้า Dict
+                analyze[i[1]] = int(check_view)
+                rate[i[1]] = float("%.1f"%check_rate)
+        check_view, check_rate = "", 0
+
+    return analyze, rate
+
+def graph(set_year, dc, marvel, name_file, name_file_all):
+    """Create a graph"""
+    """สร้างกราฟทั้งแบบ แท่ง และ วงกลม"""
+
+    """ กราฟ แท่ง (คนดู) """
+    line_chart = pygal.Bar()
+    line_chart.title = 'Marvel & DC (เรตติ้งคนดูในแต่ละปี)'
+    line_chart.x_labels = map(str, range(min(set_year), max(set_year)+1))
+    line_chart.add('DC', dc)
+    line_chart.add('Marvel', marvel)
+    line_chart.render_to_file(name_file)
+
+    """ กราฟ วงกลม (คนดู)"""
+    pie_chart = pygal.Pie()
+    pie_chart.title = 'รวมเรตติ้งคนดู ตั้งแต่ปี 2005-2018'
+    pie_chart.add('DC', dc)
+    pie_chart.add('Marvel', marvel)
+    pie_chart.render_to_file(name_file_all)
+
+    #หมายเหตุ โค้ดนี้ไม่ได้กดรันแล้วกราฟจะแสดงขึ้นมาทันที่แค่เป็นการสร้างไฟล์กราฟขึ้นมา
+
+main(set(), dict(), dict(), dict())
